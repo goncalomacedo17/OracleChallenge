7183209844c869b90d2357a0eccd8714004faa79
Gonçalo Macedo <joaog.macedo@hotmail.com>
2024-01-10 16:36:56 +0000

    fourth commit

--- src/test/java/com/interview/shoppingbasket/CheckoutPipelineTest.java
+++ src/test/java/com/interview/shoppingbasket/CheckoutPipelineTest.java
@@ -45,6 +45,7 @@ public class CheckoutPipelineTest {
 
         when(pricingService.getPrice("product1")).thenReturn(4.0);
         when(pricingService.getPrice("product2")).thenReturn(4.0);
+        when(pricingService.getPrice("product3")).thenReturn(10.0);
 
 
     }
@@ -65,37 +66,41 @@ public class CheckoutPipelineTest {
         Promotion p1 = new Promotion("product1", Promotion.PromotionType.p_50_pct);
         //Promotion p2 = new Promotion("product2", Promotion.PromotionType.p_2_for_1);
         Promotion p2 = new Promotion("product2", Promotion.PromotionType.p_2_for_1);
-
+        Promotion p3 = new Promotion("product3", Promotion.PromotionType.p_10_pct);
         //System.out.println(p1.getProductCode() + " - " + p1.getPromotionType());
 
         List<Promotion> promotions = new ArrayList<>();
         promotions.add(p1);
         promotions.add(p2);
-
+        promotions.add(p3);
         //for(Promotion p : promotions) System.out.println(p.getProductCode() + " - " + p.getPromotionType());
 
         basket.add("product1", "myproduct1", 10);
         basket.add("product2", "myproduct2", 10);
-
+        basket.add("product3","myproduct3",5);
+        basket.add("product1", "myproduct1", 2);
 
         when(promotionsService.getPromotions(basket)).thenReturn(promotions);
 
 
         System.out.println("Product1 Price: " + pricingService.getPrice("product1"));
         System.out.println("Product2 Price: " + pricingService.getPrice("product2"));
+        System.out.println("Product3 Price: " + pricingService.getPrice("product3"));
         for(Promotion p : promotionsService.getPromotions(basket)){
             System.out.println("Promotions: " + p.getProductCode() + " - " + p.getPromotionType());
 
         }
 
+        checkoutStep0 = new BasketConsolidationCheckoutStep();
         checkoutStep1 = new PromotionsCheckoutStep(promotionsService);
         checkoutStep2 = new RetailPriceCheckoutStep(pricingService);
 
+        checkoutPipeline.addStep(checkoutStep0);
         checkoutPipeline.addStep(checkoutStep1);
         checkoutPipeline.addStep(checkoutStep2);
         paymentSummary = checkoutPipeline.checkout(basket);
 
-        assertEquals(paymentSummary.getRetailTotal(),2.0 * 10 + 4.0 * 5);
+        assertEquals(paymentSummary.getRetailTotal(),2.0 * 12 + 4.0 * 5 + 9.0 * 5);
 
 
 
